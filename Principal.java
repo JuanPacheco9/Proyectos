a9fe4393af57df4282d7f36caf4ff2a75fff9d34
JuanPacheco9 <36554015+JuanPacheco9@users.noreply.github.com>
2019-07-18 21:01:24 -0500

    Add files via upload

---
+++ Principal.java
@@ -0,0 +1,33 @@
+package juego2;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.JFrame;
+
+public class Principal {
+       
+    public static void main(String[] args) {
+        m1();
+    }
+  public static void m1(){
+            
+        JFrame ventana = new JFrame("Jueguito");
+        Juego jueguito = new Juego();
+        ventana.add(jueguito);
+	ventana.setSize(1300,400);
+	ventana.setLocation(70, 200);
+        ventana.setVisible(true);
+        ventana.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	
+        while (!jueguito.juegoFinalizado) {
+            jueguito.repaint();
+            try {
+                Thread.sleep(5);
+            } catch (InterruptedException ex) {
+                Logger.getLogger(Principal.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
+        jueguito.repaint();
+    
+  }
+}
